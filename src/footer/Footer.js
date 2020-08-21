9b907720a441cf7cf75c6754b86da25b95b0b34a
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-08-21 23:39:24 +0300

    Add Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import styleContainer from "../common/styles/Container.module.css"
+import style from "../footer/Footer.module.css"
+import FooterElement from "./footerElement/FooterElement";
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className={style.footerBlock}>
+            <div className={`${styleContainer.container} ${style.footerBlock}`}>
+                <h3 className={style.title}>Artem Ermakov</h3>
+                <div className={style.foot}>
+                    <FooterElement />
+                    <FooterElement />
+                    <FooterElement />
+                    <FooterElement />
+                </div>
+                <span className={style.copyright}>© 2020 copyright all right reserved.</span>
+            </div>
         </div>
     )
 }
---
+++ src/footer/footerElement/FooterElement.js
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "../footerElement/FooterElement.module.css"
+
+const FooterElement = () => {
+    return (
+        <div className={style.footerElement}>
+            <div className={style.icon}>
+
+            </div>
+        </div>
+    )
+}
+
+export default FooterElement;
