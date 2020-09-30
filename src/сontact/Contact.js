6b9be0a46e50e4f5e13d9bc07bf1596db907224f
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-09-30 17:39:43 +0300

    add feedback fields

--- src/сontact/Contact.js
+++ src/сontact/Contact.js
@@ -1,21 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Contact.module.scss"
 import styleContainer from "../common/styles/Container.module.css"
 import Title from "../common/components/title/Title";
 import Zoom from "react-reveal";
+import axios from "axios"
+import Swal from 'sweetalert2'
+import {useFormik} from "formik";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+
+
 
 const Contact = () => {
+
+    const [preload, setPreload] = useState(null)
+
+    const [buttonSend, setButtonSend] = useState(true)
+
+    const doneAlert = () => {
+        Swal.fire(
+            `Спасибо, ${formik.values.name}!
+            Я свяжусь с вами в ближайшее свободное время.`
+        )
+    }
+
+    const initialValues = {
+        name: "",
+        contacts: "",
+        message: "",
+    }
+
+    const onSubmit = values => {
+        setPreload(<CircularProgress color={"secondary"} />)
+        axios.post("http://localhost:3010/sendMessage", {
+            name: formik.values.name,
+            contacts: formik.values.contacts,
+            message: formik.values.message
+        })
+            .then(() => {
+                doneAlert();
+                setPreload(null)
+            })
+        values.preventDefault()
+    }
+
+    const validate = values => {
+        let errors = {}
+        if (!values.name) {
+            errors.name = "Поле не может быть пустым."
+        }
+        if (!values.contacts) {
+            errors.contacts = "Поле не может быть пустым."
+        }
+        if (!values.message) {
+            errors.message = "Поле не может быть пустым."
+        }
+        if(values.name && values.contacts && values.message){
+            setButtonSend(false)
+        }
+        if(!values.name || !values.contacts || !values.message){
+            setButtonSend(true)
+        }
+        return errors;
+    }
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    })
+
     return (
         <div id={"contact"} className={style.contactBlock}>
             <div className={`${styleContainer.container} ${style.contactContainer}`}>
                 <Title title={"Contact me"}/>
                 <Zoom bottom>
                     <div className={style.contact}>
-                        <form className={style.form}>
-                            <input className={style.name} type="text" placeholder="Name?"/><br/>
-                            <input className={style.email} type="text" placeholder="Email?"/><br/>
-                            <textarea className={style.textarea} placeholder="Your message?"/><br/>
-                            <button className={style.button} type={"submit"} onClick={() => alert("Hello")}>Send Message</button>
+                        <form onSubmit={onSubmit} id={"contactForm"} className={style.form}>
+                            {preload}
+                            <input value={formik.values.name} onChange={formik.handleChange} name={"name"} onBlur={formik.handleBlur} className={style.name} type="text" placeholder="Name?"/><br/>
+                            {formik.touched.name && formik.errors.name ? <div className={style.error}>{formik.errors.name}</div> : null}
+                            <input value={formik.values.contacts} onChange={formik.handleChange} name={"contacts"} onBlur={formik.handleBlur} className={style.email} type="email" placeholder="Email"/><br/>
+                            {formik.touched.contacts && formik.errors.contacts ? <div className={style.error}>{formik.errors.contacts}</div> : null}
+                            <textarea value={formik.values.message} onChange={formik.handleChange} name={"message"} onBlur={formik.handleBlur} className={style.textarea} placeholder="Your message?"/><br/>
+                            {formik.touched.message && formik.errors.message ? <div className={style.error}>{formik.errors.message}</div> : null}
+                            <button disabled={buttonSend} className={style.button} type="submit">Send Message</button>
                         </form>
                     </div>
                 </Zoom>
@@ -23,5 +92,6 @@ const Contact = () => {
         </div>
     )
 }
+
 
 export default Contact;
